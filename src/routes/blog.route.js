58c6eaaacadcd9048799df0205ded701b5100a65
tuyenlt <keplerlu@gmail.com>
2025-04-19 12:06:00 +0700

    update

--- src/routes/blog.route.js
+++ src/routes/blog.route.js
@@ -2,5 +2,5 @@ const router = require('express').Router();
 const blogController = require('../controllers/blog.controller')
 
 router.get('/blog', blogController.getAllPosts);
-
+router.get('/blog/:id', blogController.getPostById);
 module.exports = router;
