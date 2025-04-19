b431f6b8a089f76e32b53bf3faff6bcfb364724d
Lê Trọng Tuyên <85683857+tuyenlt@users.noreply.github.com>
2025-04-19 04:55:03 +0000

    update

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const routes = require("./routes");
-const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Blog API");
